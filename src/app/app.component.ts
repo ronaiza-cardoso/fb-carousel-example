557d3e87016fdf3ea014a5e9a50deaa6f7e44471
yesroh <ronaizacard@gmail.com>
2017-05-31 17:49:10 -0300

    Fb-carousel exemple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { FbCarousel } from 'fb-carousel';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor() { }
+
+    banners: any[] = [
+    { id: 0, bg: './assets/banner.jpg', title: 'Banner 1', exibir_title: false },
+    { id: 1, bg: './assets/banner.jpg', title: 'Banner 2', exibir_title: false },
+    { id: 2, bg: './assets/banner.jpg', title: 'Banner 3', exibir_title: false },
+    { id: 3, bg: './assets/banner.jpg', title: 'Banner 4', exibir_title: false },
+  ];
+
+  arrows: any = {
+    left: '/assets/left.svg',
+    right: '/assets/right.svg'
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FbCarousel } from 'fb-carousel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FbCarousel
   ],
   imports: [
     BrowserModule,
